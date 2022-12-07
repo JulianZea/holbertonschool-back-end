75323641ed79add378cc05d966725494e86d53e3
JulianZea <cesarjulianzeabravo@gmail.com>
2022-12-07 00:00:34 -0500

    fix file 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,31 +4,31 @@ from requests import get
 from sys import argv
 
 
-def apiRest():
+def api_rest():
     ''' Gather data from an API '''
-    idUser = int(argv[1])
-    nameEmploye = ''
-    numberTask = 0
-    totalNumberTask = 0
-    listTask = []
+    id_user = int(argv[1])
+    name_employe = ''
+    number_task = 0
+    total_number_task = 0
+    list_task = []
 
-    urlUsers = get('https://jsonplaceholder.typicode.com/users').json()
-    for varuser in urlUsers:
-        if varuser['id'] == idUser:
-            nameEmploye = varuser['name']
+    url_users = get('https://jsonplaceholder.typicode.com/users').json()
+    for user in url_users:
+        if user['id'] == id_user:
+            name_employe = user['name']
             break
 
-    urlTask = get('https://jsonplaceholder.typicode.com/todos').json()
-    for vartask in urlTask:
-        if vartask['userId'] == idUser:
-            if vartask['completed'] is True:
-                listTask.append(vartask['title'])
-                numberTask += 1
-            totalNumberTask += 1
-    print('Employee {} is done with tasks({}/{}):'.format(nameEmploye,
-                                                          numberTask,
-                                                          totalNumberTask))
-    for title in listTask:
+    url_task = get('https://jsonplaceholder.typicode.com/todos').json()
+    for task in url_task:
+        if task['userId'] == id_user:
+            if task['completed'] is True:
+                list_task.append(task['title'])
+                number_task += 1
+            total_number_task += 1
+    print('Employee {} is done with tasks({}/{}):'.format(name_employe,
+                                                          number_task,
+                                                          total_number_task))
+    for title in list_task:
         print('\t {}'.format(title))
 
 
